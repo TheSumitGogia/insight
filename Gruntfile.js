db7d00222f2c5555c4c6c96c2726d421279b5c19
Sumit Gogia <sumitg.gogia0@gmail.com>
2016-02-10 20:46:12 -0500

    added exporting for lightweight release

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,14 +20,37 @@ module.exports = function(grunt) {
       }
     },
 
-    clean: ["./build"],
+    clean: {
+      build: ["./build"],
+      export: {
+        options: { force: true },
+        src: ["../export"]
+      }
+    },
+
     copy: {
       html: { src: "dom/insight.html", dest: "build/insight.html" },
       images: { expand: true, cwd: "images", src: "**", dest: "build/images/" },
       styles: { expand: true, cwd: "styles", src: "**", dest: "build/styles/" },
-      semantic: { expand: true, cwd: "bower_components/semantic-ui/dist", src: ["*.*", "**/*.*"], dest: "build/styles/semantic" }
+      semantic: { expand: true, cwd: "bower_components/semantic-ui/dist", src: ["*.*", "**/*.*"], dest: "build/styles/semantic" },
+      expBuild: { expand: true, cwd: "build", src: "**", dest: "../export/build" },
+      expTest: { expand: true, cwd: "../test", src: "**", dest: "../export/test" },
+      expScripts: { expand: true, flatten: true, src: "scripts/**/*.py", dest: "../export/scripts" }
     },
     
+    compress: {
+      export: {
+        options: {
+          archive: "../export.tar.gz",
+          mode: "tgz"
+        },
+        expand: true,
+        cwd: "../export/",
+        src: ["**"],
+        dest: "export" 
+      }
+    },
+
     requirejs: {
       build: {
         options: {
@@ -53,15 +76,24 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-requirejs");
 
+  grunt.loadNpmTasks("grunt-contrib-compress");
+
   grunt.registerTask("lint", ["jshint"]);
 
   grunt.registerTask("build", [
-    "clean", 
+    "clean:build", 
     "copy:html", 
     "copy:images", 
     "copy:styles", 
     "copy:semantic", 
     "requirejs:build"
+  ]);
+  grunt.registerTask("export", [
+    "clean:export",
+    "copy:expBuild",
+    "copy:expTest",
+    "copy:expScripts",
+    "compress:export"
   ]);
   grunt.registerTask("default", ["build"]);
 
